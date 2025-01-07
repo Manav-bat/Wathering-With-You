d04fde4e05398b6a11569f6d7d878476a6ddc8d8
yashveer98 <64112276+yashveer98@users.noreply.github.com>
2025-01-07 18:57:33 +0530

    added proxy server

---
+++ proxy/index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import axios from 'axios';
+import cors from 'cors'
+const app = express();
+
+app.use(cors())
+app.get('/timezone', async (req, res) => {
+    try {
+        const response = await axios.get('http://api.geonames.org/timezoneJSON', {
+            params: {
+                lat: req.query.lat,
+                lng: req.query.lng,
+                username: 'shinzoo_if'
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).send('Error fetching data');
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Proxy running on port 3000');
+});
